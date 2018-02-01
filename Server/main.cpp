c3be10567a8d22054acc3b6fbbb04b5a3da04218
James Muldrow <jamesmuldrow@gmail.com>
2018-01-31 20:37:26 -0500

    Added header files to server.

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,4 +1,12 @@
-include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <iostream>
 using namespace std;
 
 int main()
